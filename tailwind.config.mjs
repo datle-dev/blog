2d1df796ae4cb05dc8246625ee109744f7163069
datle-dev <dle.dev@outlook.com>
2024-07-27 18:35:46 -0400

    Remove backticks from inline code tags

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,21 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            'code::before': {
+              content: '"" !important' ,
+            },
+            'code::after': {
+              content: '"" !important',
+            },
+          },
+        },
+      },
+    },
+  },
 	plugins: [
     require('@tailwindcss/typography'),
   ],
