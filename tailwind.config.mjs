ee11355220432ffc5bf7c0568eacb610394e4e82
datle-dev <dle.dev@outlook.com>
2024-07-27 18:35:32 -0400

    Add typography plugin to tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,8 +2,8 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-	},
-	plugins: [],
+	plugins: [
+    require('@tailwindcss/typography'),
+  ],
   darkMode: 'class',
 }
