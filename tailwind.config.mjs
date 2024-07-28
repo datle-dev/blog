55d0e4170efb1d2527392f902d3f0c1440563280
datle-dev <dle.dev@outlook.com>
2024-07-28 06:33:10 -0400

    Configure tailwind typography default font families

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
     extend: {
+      fontFamily: {
+        'mono': ['JetBrains Mono', ...defaultTheme.fontFamily.mono],
+        'sans': ['IBM Plex Sans', ...defaultTheme.fontFamily.sans],
+        'sans-headings': ['IBM Plex Sans Condensed', ...defaultTheme.fontFamily.sans],
+      },
       typography: {
         DEFAULT: {
           css: {
