966379296e1b32b87354e810fc5dda7808086716
datle-dev <dle.dev@outlook.com>
2024-07-28 10:27:46 -0400

    Update blog post schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,10 +4,11 @@ const blogPostCollection = defineCollection({
   type: "content",
   schema: z.object({
     title: z.string(),
-    draft: z.boolean(),
+    draft: z.boolean().optional().default(false),
     description: z.string(),
     tags: z.array(z.string()),
     pubDate: z.date(),
+    series: z.string().optional(),
   }),
 });
 
